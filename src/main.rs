37da45da335265a0def08ec6591c4dd67e592338
edd-breaks-things <edward@schauman-haigh.dev>
2025-11-15 07:40:20 +0100

    Fix

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!("Hello, world!");
-    
+
     // Example of using a function from lib.rs
     let result = example_rust_app::add(2, 2);
     println!("2 + 2 = {}", result);
